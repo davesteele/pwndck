a63d5cc6be3fc9a7948ae6cfe0cccc50ad19e5ff
David Steele <dsteele@gmail.com>
2025-12-26 13:23:53 -0500

    Publish process_pw at root

--- pwndck/__init__.py
+++ pwndck/__init__.py
@@ -1,4 +1,5 @@
 # The external endpoint is process_pw
 
 from pwndck.flexi_formatter import FlexiHelpFormatter  # noqa
-from pwndck.pwndck import PwndException, main, process_pw  # noqa
+from pwndck.pwndck import PwndException, main  # noqa
+from pwndck.pwndck import process_pw as process_pw  # noqa
