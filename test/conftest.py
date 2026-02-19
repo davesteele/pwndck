b78d7226c918d98f290aeb4af2f55f05586b0999
David Steele <dsteele@gmail.com>
2026-02-19 16:52:50 -0500

    Run only non-internet tests by default

---
+++ test/conftest.py
@@ -0,0 +1,11 @@
+allowed_markers = ("webtest", )
+
+def pytest_collection_modifyitems(items, config):
+    # add `always_run` marker to all unmarked items
+    for item in items:
+        if not any(x.name in allowed_markers for x in item.iter_markers()):
+            item.add_marker("always_run")
+
+    # run "always_run" if no marker spec'd
+    if not config.getoption('markexpr', 'False'):
+        config.option.markexpr = "always_run"
